bfc3f576f24ebdcb6e12010fff38688749453376
caprarimihai <caprarimihail@gmail.com>
2026-08-26 00:20:21 +0300

    Initial commit

---
+++ src/main/java/robot_legendar/pixelacademy/PixelacademyApplication.java
@@ -0,0 +1,13 @@
+package robot_legendar.pixelacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelacademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelacademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/robot_legendar/pixelacademy/PixelacademyApplicationTests.java
@@ -0,0 +1,13 @@
+package robot_legendar.pixelacademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelacademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
